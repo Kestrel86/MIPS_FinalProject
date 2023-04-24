5f64ec470744c9ac23fe3da5ea3b4a59e5e7909f
Kestrel86 <valdezandrew20@gmail.com>
2023-04-24 10:33:26 -0700

    set up minor idea of bubble sort

--- bubbleSort.asm
+++ bubbleSort.asm
@@ -1,1 +1,26 @@
 #bubble sort test test
+
+
+
+.data
+# 1 or 2 arrays to test with
+
+.text
+main:
+
+loop:
+#bubble sort is made to iterate through an array,
+#swapping values next to each other if they are smaller or large
+#can take a ton of time looping until all values have been swapped to 
+#a correct organized array
+
+#Loop constantly, checking if the next value is bigger
+#or smaller than the one before, if smaller, 
+#then swap with the value to the left
+#continuing until the end (1 pass)
+
+#loop until all is sorted
+
+exit:
+	li $v0, 10
+	syscall
