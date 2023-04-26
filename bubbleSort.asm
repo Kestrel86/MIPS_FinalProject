54f5bfe72464b309ff66efa9d4c57ec16bf0dfec
YoussefCS <43261131+YoussefCS@users.noreply.github.com>
2023-04-26 15:03:06 -0700

    Add files via upload
    Updated by Youssef

--- bubbleSort.asm
+++ bubbleSort.asm
@@ -1,13 +1,30 @@
 #bubble sort test test
-
-
-
 .data
-# 1 or 2 arrays to test with
-
+array1: .word 8, 7, 2, 3, 5, 10, 1, 4, 6, 9
+array2: .word 100, 35, 56, 25, 87, 15, 9
+message: .asciiz "Unsorted Array:\n1-(8, 7, 2, 3, 5, 10, 1, 4, 6, 9)\n2- (100, 35, 56, 25, 87, 15, 9)"
+sort: .asciiz "Sorted Array: "
 .text
 main:
-
+	#print the first message
+	li $v0, 4
+	la $a0, message
+	syscall
+	
+	#load arrays in the address
+	la $s0, array1
+	la $s1, array2
+	
+	#initalize loop counter $t0 array1
+	move $t0, $zero
+	#initalize loop counter $t0 array2
+	move $t1, $zero
+	
+	#print the first message
+	li $v0, 4
+	la $a0, sort
+	syscall
+	
 loop:
 #bubble sort is made to iterate through an array,
 #swapping values next to each other if they are smaller or large
