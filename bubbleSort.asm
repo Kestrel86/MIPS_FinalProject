5c9e5dc8a435a30b4821c57160842c0d1d1e8e8a
j-e-ff <rodasjeffrey@gmail.com>
2023-05-03 13:24:54 -0700

    Update bubbleSort.asm

--- bubbleSort.asm
+++ bubbleSort.asm
@@ -4,10 +4,14 @@
 # This program will:
 # Use the bubble sort algorithm to sort two hard-coded arrays
 # Print both arrays before nad after sorting.
+# reference: https://github.com/acai422/Bubble-Sort/blob/master/BubbleSort.asm
 
 
 # Applies the bubble sort algorithm to the given array with the given array size.
 .macro bubbleSort(%arrayLabel, %arraySize)
+	#storing user input to decide which sort to do
+	add $t5, $t5, $t0
+	
 	#load arrays in the address
 	la $s7, %arrayLabel
 	
@@ -17,8 +21,14 @@
 
 	#initialize "limit" for loops at $s6.  This limit will be adjusted accordingly, but it should start as arraySize.
 	li $s6, %arraySize
+	j whichLoop
 	
-loop:
+whichLoop:
+	#deciding which sort to do 
+	beq $t5, 1, loop 
+	beq $t5, 2, loop2
+	
+loop: #low to high
 	sll $t7, $s1, 2		#multiply $s1 by 2 and put it in t7
 	add $t7, $s7, $t7 	#add the address of numbers to t7
 
@@ -28,7 +38,24 @@ loop:
 	slt $t2, $t0, $t1	#if t0 < t1
 	bne $t2, $zero, increment
 
-	sw $t1, 0($t7) 		#swap
+	#swap
+	sw $t1, 0($t7) 		
+	sw $t0, 4($t7)
+	
+	j loop
+	
+loop2: #high to low
+	sll $t7, $s1, 2		#multiply $s1 by 2 and put it in t7
+	add $t7, $s7, $t7 	#add the address of numbers to t7
+
+	lw $t0, 0($t7)  		#load numbers[j]	
+	lw $t1, 4($t7) 		#load numbers[j+1]
+
+	sgt $t2, $t0, $t1	#if t0 > t1
+	bne $t2, $zero, increment
+
+	#swap
+	sw $t1, 0($t7) 		
 	sw $t0, 4($t7)
 
 increment:	
@@ -36,7 +63,7 @@ increment:
 	addi $s1, $s1, 1		#increment s1
 	sub $s5, $s6, $s0 	#subtract s0 from s6 ( $s5 = # of elements - # of total passes)
 
-	bne  $s1, $s5, loop		# if s1 (counter for second loop) does not equal # of elements - # of total passes - 1, loop
+	bne  $s1, $s5, whichLoop		# if s1 (counter for second loop) does not equal # of elements - # of total passes - 1, loop
 					# This case simply moves the pointer again over 1.  
 					# In other words, goes to the next iteration of the inner "swap" loop
 							
@@ -45,7 +72,7 @@ increment:
 	addi $s0, $s0, 1 		#otherwise add 1 to s0
 	li $s1, 0 			#reset s1 to 0
 
-	bne  $s0, $s6, loop		# go back through loop with s0 = s0 + 1
+	bne  $s0, $s6, whichLoop		# go back through loop with s0 = s0 + 1
 .end_macro
 
 
@@ -61,7 +88,7 @@ increment:
 	# Initialize loop counter + starting address
 	move $t3, $zero
 	la $t4, %arrayLabel
-
+	add $t4, $t5, $t4 	#used to avoid address if sort from high to low
 	# Print an open bracket
 	printString(arrayOpenBracket)
 
@@ -102,12 +129,12 @@ outPrintArrayLoop:
 arr: .space 40
 unsortedMessage: .asciiz "Unsorted Array: "
 sortedMessage: .asciiz "\nSorted Array: "
-arrayOneMessage: .asciiz "\nArray: "
 arrayOpenBracket: .asciiz "["
 arrayClosedBracket: .asciiz "]"
 arrayCommaSpace: .asciiz ", "
 enter: .asciiz "Enter 10 integers: "
-option1: .asciiz "\nEnter 1 to input an array or 2 to exit program: "
+option1: .asciiz "\nEnter 1 to sort array(low to high), 2 sort (high to low), or 3 to exit program: "
+errorMessage: .asciiz "Please input a number from the given options"
 
 .text
 main:
@@ -120,25 +147,30 @@ Menu:
     	syscall
     	move $t0, $v0
     	#comparing user input
-    	beq $t0,2,exit
+    	beq $t0,3,exit
+    	bgt $t0, 3, error
+    	blt $t0, 0, error
     	#print message and getting array
     	printString(enter)
-    	beq $t0,1,input
-    	
+    	j input
     	
 input: 	
-	beq $s3,40,message
+	#getting user input (10 integers)
+	beq $s3,40,oneOrTwo
 	li $v0,5
     	syscall
     	sw $v0,arr($s3)
     	add $s3,$s3,4
 	j input
 	
-message:
-	#print the first message
-	printString(unsortedMessage)
+oneOrTwo: 
+	#finding which sort it should do
+	beq $t0, 1, lowToHigh
+    	beq $t0, 2, highToLow
+	
+lowToHigh:
 	# Print the first array (unsorted)
-	printString(arrayOneMessage)
+	printString(unsortedMessage)
 	printArray(arr, 10)
 	
 	#array
@@ -147,13 +179,37 @@ message:
 	# Print the sorted message
 	printString(sortedMessage)
 	
-	# Print the sorted array
-	printString(arrayOneMessage)
+	# Initialize loop counter + starting address
+	li $t5, 0 #initializing $t5 for the print array
 	printArray(arr, 10)
 	
 	#reset register s3
 	li $s3,0
 	
+	j Menu
+	
+highToLow:
+	#print the first message
+	printString(unsortedMessage)
+	printArray(arr, 10)
+	
+	#array
+	bubbleSort(arr, 10)
+	
+	# Print the sorted message
+	printString(sortedMessage)
+	
+	# Initialize loop counter + starting address
+	li $t5, 4 #avoiding the address of the array
+	printArray(arr, 10)
+	
+	#reset register s3
+	li $s3,0
+	
+	j Menu
+	
+error: 
+	printString(errorMessage)
 	j Menu
 exit:
 	li $v0, 10
